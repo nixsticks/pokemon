7561167697d7f0eaa2cf0a7809d7e0e1dd1b7136
Nikki Thean <nikki.thean@gmail.com>
2013-11-21 20:39:27 -0500

    Add specs

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require 'awesome_print'
 require './scraper'
+require './pokemon'
+require 'ruby-debug'
 
 scraper = Scraper.new("http://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number")
 pokemon = scraper.get_pokemon_urls
 
+pokedex = []
+
 pokemon.each do |url|
   scrape = Scraper.new(url)
   name = scrape.get_pokemon_name
-  file = File.open( "#{name}.rb", "w" )
-  file << "class #{name}\n\nend"
-  file.close  
+  debugger
+  pokedex << Pokemon.new(name)
+  # file = File.open( "#{name}.rb", "w" )
+  # file << "class #{name}\n\nend"
+  # file.close  
 end
+
+ap pokedex
---
+++ spec/pokemon_spec.rb
@@ -0,0 +1,17 @@
+require '../pokemon'
+
+describe Pokemon do
+  let(:pokemon) {Pokemon.new("Jigglypuff")}
+
+  describe '#initialize' do
+    it 'should initialize the Pokemon with its name' do
+      expect(pokemon.name).to eq("Jigglypuff")
+    end
+  end
+
+  describe '#say_name' do
+    it 'should say its name' do
+      expect(pokemon.say_name).to eq('I am Jigglypuff! ROAR')
+    end
+  end
+end
