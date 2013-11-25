8ef58288732b0f92b9dbd3b9303dfb86858943ca
Nikki Thean <nikki.thean@gmail.com>
2013-11-24 23:18:48 -0500

    Refactor

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App
   attr_reader :pokedex
 
   def initialize
-    File.open("./pokedex.yaml", "r") do |file|
+    File.open("./pokedex2.yaml", "r") do |file|
       @pokedex = YAML::load(file)
     end
   end
@@ -18,7 +18,7 @@ class App
   end
 
   def lookup_message
-    "\nType in the name of a Pokémon to get its information."
+    "\nType in the name of a Pokémon to get its information. Print e to exit at any time."
   end
 
   def get_input
@@ -27,6 +27,8 @@ class App
 
   def lookup
     entry = get_input
+
+    exit if /^e(xit)?$/.match(entry)
 
     pokedex.each do |pokemon|
       return pokemon if pokemon.name.downcase == entry
@@ -38,47 +40,34 @@ class App
 
   def print_information(pokemon)
     puts
-    pokemon.instance_variables.each do |variable|
-      var_name = variable.to_s.gsub("@","")
-      result = pokemon.send("#{var_name}")
-      debugger
-      if result.is_a?(Array)
-        puts var_name.capitalize
-        ap result
-      else
-        puts var_name.capitalize + ": " + result
-      end
-    end
-    # puts "Name: #{pokemon.name}"
-    # puts "Type: #{pokemon.type}"
-    # puts "Learnset:"
-    # ap pokemon.learnset
+    # pokemon.instance_variables.each do |variable|
+    # #   var_name = variable.to_s.gsub("@","")
+    #   result = pokemon.send("#{var_name}")
+    #   debugger
+    #   if result.is_a?(Array)
+    #     puts var_name.capitalize
+    #     ap result
+    #   else
+    #     puts var_name.capitalize + ": " + result
+    #   end
+    # end
+    puts "Name: #{pokemon.name}"
+    puts "Type: #{pokemon.type}"
+    puts "Learnset:"
+    ap pokemon.learnset
   end
 
   def another_pokemon_message
-    "\nWould you like to look up another entry?"
+    "\nLook up another entry?"
   end
 
-  def another_pokemon
-    case get_input
-    when /^y(es)?$/
-      return
-    when /^no?$/
-      puts "\nGoodbye!"
-      exit
-    else
-      puts "Please type yes or no."
-      another_pokemon
-    end
-  end
 
   def run
     puts welcome
+    puts lookup_message
     loop do
-      puts lookup_message
       print_information(lookup)
       puts another_pokemon_message
-      another_pokemon
     end
   end
 end
